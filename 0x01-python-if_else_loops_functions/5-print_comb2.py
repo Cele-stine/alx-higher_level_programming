a4e72289c69235a834b2c9496047f8b264ef7577
Celestine Wangechi <wangechicelestine@gmail.com>
2023-08-08 17:17:26 -0700

    Print 00..99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 for num in range(00, 100):
-    print("{:02d},".format(num), end=" ")
-print()
+    if num == 99:
+        print("{}".format(num))
+    else:
+        print("{:02d},".format(num), end=" ")
