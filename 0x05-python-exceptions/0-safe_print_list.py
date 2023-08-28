df757a6cae940f173f39ec2a37e1d41094bc4509
Celestine Wangechi <wangechicelestine@gmail.com>
2023-08-28 04:31:00 -0700

    Handling Exeptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,8 +3,8 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
+            print("{}".format(my_list[i]), end="")
             count += 1
-            print("{}".format(my_list[x]), end="")
         except IndexError:
             print("indexing error")
         print("")
