55fd1bdef7406e775562e314f69a3aed69567f98
Celestine Wangechi <wangechicelestine@gmail.com>
2023-08-28 06:12:17 -0700

    Print only integers whilwe handling exceptions

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,9 +3,9 @@ def safe_print_list_integers(my_list=[], x=0):
     ret = 0
     for i in range(0, x):
         try:
-            print("{:d}".format(my_list[i], end=""))
+            print("{:d}".format(my_list[i]), end="")
             ret += 1
         except (ValueError, TypeError):
             continue
-        print("")
-        return ret
+    print("")
+    return (ret)
