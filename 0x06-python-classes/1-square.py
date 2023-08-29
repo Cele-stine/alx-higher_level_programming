42f00d2dea99b63556b477ce7928f96491799f5a
Celestine Wangechi <wangechicelestine@gmail.com>
2023-08-29 15:00:56 -0700

    Requirment Fail

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,4 +2,9 @@
 """Add a Private Attrinbute to the class Square"""
 class Square:
     def __init__(self, size):
+          """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         self.__size = size
