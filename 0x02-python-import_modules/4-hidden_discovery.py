c5f65cbea9249a4ea4e9555b7a1ea454745badac
Celestine Wangechi <wangechicelestine@gmail.com>
2023-08-29 05:44:28 -0700

    Hidden discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-import hidden_4
+
 if __name__ == "__main__":
-    def user_defined(obj):
-        return not (obj.__name__ in dir(__builtins__))
-    def print_names(hidden_4):
-        def_names = dir(hidden_4)
-        filt_names = sorted(name for name in def_names if not name.startswith('__') and user_defined(getattr(hidden_4, name)))
-        for name in filt_names:
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
             print(name)
