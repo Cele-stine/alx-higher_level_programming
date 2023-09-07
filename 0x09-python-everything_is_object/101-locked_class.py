1c8230facc0f60a64157a4d5a8e7e10348a303ce
Celestine Wangechi <wangechicelestine@gmail.com>
2023-09-07 00:47:46 -0700

    Solving the pycode violation.

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -3,6 +3,6 @@
 
 
 class LockedClass:
-    """__slots__ is a Python feature that can be used to restrict attributes that can be added to instances of your class."""
+    """__slots__ to restrict attributes added to instances of your class."""
 
     __slots__ = ["first_name"]
