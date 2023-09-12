5a5c72df620c7884675277d8db7f343dfdea15df
Celestine Wangechi <wangechicelestine@gmail.com>
2023-09-12 14:45:16 -0700

    Append a string.

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""Define a function append_write."""
+
+
 def append_write(filename="", text=""):
+    """Append a string to a file.
+
+    Args:
+        filename: the file
+        text: the string to append.
+    """
+
     with open(filename, 'a', encoding='utf-8') as filename:
         filename.write(text)
         return len(text)
