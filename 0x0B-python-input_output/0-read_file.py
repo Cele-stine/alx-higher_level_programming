791d8407c3534e964e5f59ea4a2afc0764a0d8e0
Celestine Wangechi <wangechicelestine@gmail.com>
2023-09-12 12:15:07 -0700

    Read from a file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define a function that reads a file and prints its content."""
+
+
+def read_file(filename=""):
+    """Open file to read.
+
+    Args:
+    filename: the name assigment of the file while opening.
+    """
+
+    with open(filename, 'r', encoding='utf-8') as filename:
+        print("{}".format(filename.read()))
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,3 @@
+         PYTHON: INPUT AND  OUTPUT.
+Hndling files ie reading and writing files are an important part of programming.
+
