1cca54c61e8e377dcb72965f99b692f3a104d326
Celestine Wangechi <wangechicelestine@gmail.com>
2023-09-13 03:42:27 -0700

    Add all argumentsto a python list.

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""Get all modules needed."""
 import sys
 import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
 """Define a function that adds all arguments to a python list."""
 
 
