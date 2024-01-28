7f6aff1575adbbda06fccab636f43b321cada295
Celestine Wangechi <wangechicelestine@gmail.com>
2024-01-28 04:08:45 -0800

    fix: Recive the URL from the terminal

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Take a URL send a request to it and display the size of the content.
-curl -sI http://www.google.com | awk '/Content-Length/ {print $2}' | tr -d '/r/n'
+curl -s "{$1}" | wc -c
